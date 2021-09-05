671603f2a501d2559153c61ad7c5b4b92219fec6
Aakriti Goyal <82526001+AakritiGoyal12458@users.noreply.github.com>
2021-09-05 22:31:19 +0530

    File updated
    Added Sleep Function

---
+++ screenshot.py
@@ -0,0 +1,34 @@
+#taking screenshot with python
+##1st black n white
+##2nd coloured
+##we will form a notification center too...
+
+##we need pyautogui
+##so pip install pyautogui
+##ImageGrab:black and white always
+##for coloured (r,g,b - red , green , blue ) 0,0,0 to 255,255,255
+
+
+import time
+from PIL import Image,ImageGrab
+
+
+def takescreenshot():#make a function
+    image=ImageGrab.grab()
+    image.show()
+    return image
+
+if __name__=="__main__":
+    time.sleep(3)
+    
+    image=takescreenshot()
+    data=image.load()
+    print(data)
+    #make  image colourfull from 0 to 255
+
+    for i in range(35,56):
+        for j in range(0,255):
+            data
+        
+    
+
